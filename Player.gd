7104ed7c78c7d5fcc4154d0bdda3e4b852e5ba9b
Pengwuin <149349271+PengwuinReal@users.noreply.github.com>
2024-03-11 08:05:41 -0700

    3-9/10-2024
    flipping side to side
    scythe attack

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -15.0
 const RESISTANCE = 0.1 # "gliding" in air
 const SLIDE = 0.7 # sliding on ground
 const CLIMB_SPEED = 5.0
+var FACING = 1 #left = -1 right = 1, for a boolean
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -21,6 +22,8 @@ func positionreset(): # when a key is pressed, will set the position and set vel
 func _input(event):
 	if Input.is_key_pressed(KEY_R): #reset character to spawn button
 		positionreset()
+	
+		
 		
 func _physics_process(delta):
 	#print(delta)
@@ -32,21 +35,33 @@ func _physics_process(delta):
 		SPEED = 20
 	else:
 		SPEED = 10
-		
-		
+	if Input.is_action_just_pressed("attack"):
+		$"Scythe".set_rotation_degrees(-50)
+	if $"Scythe".get_rotation_degrees() <= 100 and Input.is_action_pressed("attack"):
+		$"Scythe".set_rotation_degrees($"Scythe".get_rotation_degrees() + 10)
+	if Input.is_action_just_released("attack") or $"Scythe".get_rotation_degrees() >= 100:
+		$"Scythe".set_rotation_degrees(0)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if direction:
+	if direction: #left = -1 right = 1
 		velocity.x = direction * SPEED
+ 		#i wanna flip this node when going left PLEASEEEEEEEEEEEEEEE FIXXXXXX
+		if Input.is_action_pressed("left") and FACING == 1: 
+			scale.x = -1
+			FACING = -1
+		if Input.is_action_pressed("right") and FACING == -1:
+			scale.x = -1
+			FACING = 1
 		#print(velocity.x) #debug
+		print(scale.x)
 	else:
 		if is_on_floor(): 
 			velocity.x = move_toward(velocity.x, 0, SLIDE)
 		if not is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, RESISTANCE)
-		
+	
 	
 	if is_on_floor() and Input.is_action_pressed("jump"):#jumping on a floor
 		velocity.y = JUMP_VELOCITY
